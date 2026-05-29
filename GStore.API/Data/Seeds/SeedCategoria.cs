65421d706655095d1f009b9ec07c79100f044ac1
7diegojr <dfsntsjunior@gmail.com>
2026-05-29 12:27:32 -0300

    incluído seeds para mock de dados no AppDbContext, configurado a conexão com banco MySQL e iniciado o suporte à autenticação JWT e Identity

--- GStore.API/Data/Seeds/SeedCategoria.cs
+++ GStore.API/Data/Seeds/SeedCategoria.cs
@@ -7,12 +7,14 @@ public class SeedCategoria
 {
     public SeedCategoria(ModelBuilder modelBuilder)
     {
-        List<Categoria> categorias = [
-            new() {id = 1, Nome = "Camisas"},
-            new() {id = 2, Nome = "Retrô"},
-            new() {id = 3, Nome = "Jaquetas e Casacos"},
-            new() {id = 4, Nome = "Acessórios"},
-        ];
+        List<Categoria> categorias = new List<Categoria>
+        {
+            new Categoria { Id = 1, Nome = "Camisas" },
+            new Categoria { Id = 2, Nome = "Retrô" },
+            new Categoria { Id = 3, Nome = "Jaquetas e Casacos" },
+            new Categoria { Id = 4, Nome = "Acessórios" }
+        };
+
         modelBuilder.Entity<Categoria>().HasData(categorias);
     }
 }
--- GStore.API/Program.cs
+++ GStore.API/Program.cs
@@ -1,6 +1,56 @@
+using GStore.API.Data;
+using GStore.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Serviço de Conexão com o Banco
+string conexao = builder.Configuration.GetConnectionString("Conexao");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(conexao)
+);
+
+// Serviço de Autenticação e Autorização - Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>(options =>
+{
+    // Configuração de Senha
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+
+    // Configuração de Bloqueio
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+    // Configurações de Usuário
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// Serviços JWT
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi

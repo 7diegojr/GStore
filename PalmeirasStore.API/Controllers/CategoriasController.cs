06a58ce36336ab82a1a9f8b32bcc0da656269c6a
7diegojr <dfsntsjunior@gmail.com>
2026-06-11 12:23:06 -0300

    Finalizado todos os métodos do Categoria Controller

--- PalmeirasStore.API/Controllers/CategoriasController.cs
+++ PalmeirasStore.API/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PalmeirasStore.API.Data;
 using PalmeirasStore.API.Models;
 
@@ -27,5 +28,47 @@ public class CategoriasController : ControllerBase
     {
         var categoria = _context.Categorias.Find(id);
         return categoria == null ? NotFound("Categoria não encontrada.") : Ok(categoria);
+    }
+
+    [HttpPost]
+    public ActionResult PostCategoria([FromBody] Categoria categoria)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Verifique os dados informados");
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+        return CreatedAtAction("GetCategoria", new { id = categoria.Id }, categoria);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult PutCategoria(int id, [FromBody] Categoria categoria)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Verifique os dados informados");
+        if (id != categoria.Id)
+            return BadRequest("Verifique os dados informados");
+        
+        var oldCategoria = _context.Categorias.Find(id);
+        if (oldCategoria == null)
+            return NotFound("Categoria não localizada");
+        oldCategoria.Nome = categoria.Nome;
+        oldCategoria.Foto = categoria.Foto ?? oldCategoria.Foto; // "??" --> significa "ou"
+        oldCategoria.Cor = categoria.Cor ?? oldCategoria.Cor;
+        _context.Entry(oldCategoria).State = EntityState.Modified;
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public ActionResult DeleteCategoria(int id)
+    {
+        var oldCategoria = _context.Categorias.Find(id);
+        if (oldCategoria == null)
+            return NotFound("Categoria não localizada");
+        // Poderia pesquisar se existem produtos antes de excluir
+        _context.Categorias.Remove(oldCategoria);
+        _context.SaveChanges();
+        return NoContent();
     }
 }
